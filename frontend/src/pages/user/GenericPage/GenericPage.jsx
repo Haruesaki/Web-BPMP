47b528e63c30bd9141507ab701946762766e8818
PaleHiu <muzakky098@gmail.com>
2026-07-06 13:37:18 +0700

    manambahkan component type default

--- frontend/src/pages/user/GenericPage/GenericPage.jsx
+++ frontend/src/pages/user/GenericPage/GenericPage.jsx
@@ -5,12 +5,7 @@ import DefaultContent from '../../../components/user/content-types/Default/Defau
 const GenericPage = () => {
   return (
     <main className="generic-page-container">
-      {/* 
-        Wadah ini akan membungkus semua jenis konten dinamis.
-        Styling seperti padding-top untuk memberi ruang dari Navbar,
-        max-width untuk keterbacaan, dan margin untuk sentralisasi
-        diatur dalam file GenericPage.css.
-      */}
+      <DefaultContent />
       <DefaultContent />
     </main>
   );
