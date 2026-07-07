abc4c11e679ff75a32079595bcd04982c26bd70f
PaleHiu <muzakky098@gmail.com>
2026-07-07 19:09:53 +0700

    Menambah Card Profile

--- frontend/src/pages/user/GenericPage/GenericPage.jsx
+++ frontend/src/pages/user/GenericPage/GenericPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './GenericPage.css';
+import CardContent from '../../../components/user/content-types/CardProfile/CardContent';
 import DefaultContent from '../../../components/user/content-types/Default/DefaultContent';
 
 const GenericPage = () => {
   return (
     <main className="generic-page-container">
-      <DefaultContent />
-      <DefaultContent />
+      <CardContent/>
+      {/* <DefaultContent/> */}
     </main>
   );
 };
